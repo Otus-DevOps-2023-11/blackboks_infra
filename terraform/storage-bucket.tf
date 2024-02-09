04dcbf1f427a7fc1bcb2dc8909733f27ad3f9f26
blackboks <blackboks@yandex.ru>
2024-02-09 14:16:00 +0300

    Homework #7 - IaC and command work with Terraform

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.107.0"
+    }
+  }
+  required_version = "1.7.0"
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_storage_bucket" "tf-bucket" {
+  access_key = "YCAJEAmejDzga2rhROakPI6z3"
+  secret_key = "YCOa587ndli2mNbRASed6MkwTeKn2r3K6NRS5Tmk"
+  bucket     = "tf-state-files"
+}
