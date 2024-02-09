2fdd09eac89bcf3001d8efe7e0669cd56e27ff8c
blackboks <blackboks@yandex.ru>
2024-02-09 14:28:12 +0300

    Homework #7 - IaC and command work with Terraform

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,20 @@
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "0.107.0"
+#     }
+#   }
+#   required_version = "1.7.0"
+# }
+
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
